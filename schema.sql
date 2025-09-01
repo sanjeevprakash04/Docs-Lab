176f983a90e7327c791a8cbe0d5184f6ff736f74
SanjeevPrakash04 <jonathan@proliteautomation.com>
2025-09-01 10:59:58 +0530

    update

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL,
-    role TEXT NOT NULL,
-    status TEXT NOT NULL,
-    last_login TIMESTAMP
-);
